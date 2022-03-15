e5cbf0088ada39c9c9655a58aadb62fbf346b503
Archana_Shinde <archanashinde113@gmail.com>
2022-03-15 13:09:20 +0530

    trans_Wallet

---
+++ model/model.wallet.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/receipt', {
+
+    useNewUrlParser: true,
+    useUnifiedTopology:true,
+}).then(function () {
+    console.log("Connection connected Successfully");
+}).catch(function () {
+    console.log("Connection Fail");
+})
+
+
+//Schema
+const ReceiptSchema = new mongoose.Schema({
+    // _id:mongoose.Schema.Types.ObjectId,
+    transaction_from: {
+        type: String,
+        required:true   
+    },
+    transaction_to:{
+
+        type:String,
+        required:true   
+
+    },
+    value:{
+
+        type:String,
+        required:true   
+        
+
+    }
+    // transaction_hash:{
+
+    //     type:String,
+    //     required:true
+    // }
+
+
+});
+//Model
+const receiptApi = mongoose.model('receiptApi', ReceiptSchema);
+module.exports =receiptApi
