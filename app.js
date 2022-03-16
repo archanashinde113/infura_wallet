4b3029834f86efde974c54915c615432abeb414d
Archana_Shinde <archanashinde113@gmail.com>
2022-03-16 11:16:00 +0530

    updated

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ const mongoose = require('mongoose')
 // const web3_bnb = new Web3('https://data-seed-prebsc-1-s1.binance.org:8545');
 const app = express()
 app.use(express.json());
-const controller= require('./controller/controller.wallet.js');
+//const controller= require('./controller/controller.wallet.js');
 //const controller= require('./controller/controller.bnb.js');
-const receiptApi = require('./model/model.wallet.js');
+//const receiptApi = require('./model/model.wallet.js');
 const bodyParser =require('body-parser');
-const port = 3000;
+const port = 4000;
 
 
 
@@ -19,17 +19,19 @@ const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }))
 // fetch a balance
-app.get('/ethbalance', controller.ethbalance);
+// app.get('/ethbalance', controller.ethbalance);
 
-// For Transaction
-app.post('/ethtransaction',controller.ethtransaction);
+// // For Transaction
+// app.post('/ethtransaction',controller.ethtransaction);
 
-// for Binance fetch balance
-app.get('/bnbBalance',controller.bnbBalance);
+// // for Binance fetch balance
+// app.get('/bnbBalance',controller.bnbBalance);
 
-//Binance For Transaction 
-app.post('/bnbTransaction',controller.bnbTransaction);
+// //Binance For Transaction 
+// app.post('/bnbTransaction',controller.bnbTransaction);
 
+
+require("./routes/router.wallet.js")(app);
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
   })
@@ -37,3 +39,6 @@ app.listen(port, () => {
 
 
 
+
+
+  
