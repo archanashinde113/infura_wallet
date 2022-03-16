60a6142f8bbbe10eef8aac08db01fd7422741532
Archana_Shinde <archanashinde113@gmail.com>
2022-03-16 10:47:45 +0530

    updated

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const dotenv = require('dotenv');
-dotenv.config();
+//const dotenv = require('dotenv');
+//dotenv.config();
 const express = require('express');
 const mongoose = require('mongoose')
-const Web3 = require("web3");
-const web3  =  new Web3("https://ropsten.infura.io/v3/9cc53a94cc4a42b793e399fd8b0d5755");
-const web3_bnb = new Web3('https://data-seed-prebsc-1-s1.binance.org:8545');
+// const Web3 = require("web3");
+// const web3  =  new Web3("https://ropsten.infura.io/v3/9cc53a94cc4a42b793e399fd8b0d5755");
+// const web3_bnb = new Web3('https://data-seed-prebsc-1-s1.binance.org:8545');
 const app = express()
 app.use(express.json());
 const controller= require('./controller/controller.wallet.js');
@@ -12,6 +12,10 @@ const controller= require('./controller/controller.wallet.js');
 const receiptApi = require('./model/model.wallet.js');
 const bodyParser =require('body-parser');
 const port = 3000;
+
+
+
+
 
 app.use(bodyParser.urlencoded({ extended: true }))
 // fetch a balance
@@ -29,3 +33,7 @@ app.post('/bnbTransaction',controller.bnbTransaction);
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
   })
+
+
+
+
